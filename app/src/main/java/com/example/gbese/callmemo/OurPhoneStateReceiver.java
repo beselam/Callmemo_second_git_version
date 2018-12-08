56039799be4e445e2012ab3a532a5bcca44244bf
nebi <snoopafr@gmail.com>
2018-12-08 20:10:57 +0200

    working on broadcast

--- app/src/main/java/com/example/gbese/callmemo/OurPhoneStateReceiver.java
+++ app/src/main/java/com/example/gbese/callmemo/OurPhoneStateReceiver.java
@@ -13,22 +13,6 @@ import android.widget.Toast;
 public class OurPhoneStateReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        try {
-            System.out.println("Receiver start");
-            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
-            if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
-                Toast.makeText(context,"Ringing State Number is -",Toast.LENGTH_SHORT).show();
-            }
-            if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))){
-                Toast.makeText(context,"Received State",Toast.LENGTH_SHORT).show();
-            }
-            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
-                Toast.makeText(context,"Idle State",Toast.LENGTH_SHORT).show();
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
